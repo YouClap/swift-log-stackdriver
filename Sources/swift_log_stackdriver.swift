0696352094c724cdc97bafa4adbad8495e02aec2
Rui Filipe de Sousa Martins <ruimartinsptl@gmail.com>
2019-05-15 18:36:09 +0100

    first version without errors, WORK IN PROGRESS

---
+++ Sources/swift_log_stackdriver.swift
@@ -0,0 +1,42 @@
+import Logging
+
+//struct swift_log_stackdriver {
+////    private let X: StackDriverHandlerRepresentable
+//
+//}
+
+
+final class StackDriverLogDestination<MetadataKey: Hashable>: MetadataLogDestination {
+    
+    let minLevel: Log.Level
+    let queue: Log.Queue
+    
+    private let stackDriverLogger: StackDriverHandlerRepresentable
+    
+    init(stackDriverLogger: StackDriverHandlerRepresentable, minLevel: Log.Level = Log.Level.info, queue: Log.Queue = Log.Queue(label: "STACKDRIVER")) {
+        // TODO: We need to set the log queue?
+        self.minLevel = minLevel
+        self.queue = queue
+        self.stackDriverLogger = stackDriverLogger
+    }
+    
+    func setMetadata(_ metadata: [MetadataKey : Any], onFailure: @escaping (Error) -> Void) {}
+    
+    func removeMetadata(forKeys keys: [MetadataKey], onFailure: @escaping (Error) -> Void) {}
+    
+    func write(item: Log.Item, onFailure: @escaping (Error) -> Void) {
+        guard item.level.rawValue >= minLevel.rawValue else { return }
+        
+        queue.dispatchQueue.sync { [weak self, item] in
+//            let _ = self?.stackDriverLogger.send(item.level.sbLevel,
+//                                                 msg: item.message,
+//                                                 thread: item.thread,
+//                                                 file: item.file,
+//                                                 function: item.function,
+//                                                 line: Int(item.line))
+            let _ = self?.stackDriverLogger.send(item: item)
+        }
+    }
+    
+
+}
